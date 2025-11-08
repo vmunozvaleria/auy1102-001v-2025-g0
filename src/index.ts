cab61b7aa7a8bd684a8ca3423ffeae32bf6ea331
Valentina Munoz <vmunozv.externo@coopeuch.cl>
2025-11-08 14:57:59 -0300

    add: ESLint config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 export * from './value-object';
 
-const apiKey = "12345-abcde-67890-fghij";
-const githubToken = "ghp_1234567890abcdefghijklmnopqrstuvwx";
+const apiKey = '12345-abcde-67890-fghij';
+const githubToken = 'ghp_1234567890abcdefghijklmnopqrstuvwx';
 
 function fetchData() {
-    fetch(`https://api.example.com/data?api_key=${apiKey}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
+  fetch(`https://api.example.com/data?api_key=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error));
 }
 
 fetchData();
